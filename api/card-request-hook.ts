ace86faf6983cd24a6738f057f8ea89530568a72
Henri <HenriChabrand@users.noreply.github.com>
2023-10-12 16:43:42 +0200

    Update card-request-hook.ts

--- api/card-request-hook.ts
+++ api/card-request-hook.ts
@@ -20,7 +20,7 @@ export default async (req: NowRequest, res: NowResponse): Promise<void> => {
                 }
             }
         );
- console.log(JSON.stringify(apiResponse))
+ console.log(apiResponse)
         // If the POST request is successful
         if(apiResponse.status === 200) {
             res.status(201).send({ message: 'Created' });
