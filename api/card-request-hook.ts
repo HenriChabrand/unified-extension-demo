c5ea9d83f6ebdd0b0a472f086b8111bb3bd80068
Henri <HenriChabrand@users.noreply.github.com>
2024-03-19 07:48:32 +0100

    Update card-request-hook.ts

--- api/card-request-hook.ts
+++ api/card-request-hook.ts
@@ -18,15 +18,15 @@ export default async (req: NowRequest, res: NowResponse): Promise<void> => {
         card.newText('Total HT (unique)',  '2400,00€');
         card.newText('Total HT (recurrent)',  '120,00€');
         card.newText('Remise HT (unique)',  '50,00€');
-        card.newText('#1', '■ (1 x 1000€) Poteau De Clôture En Acier Ga...');
-        card.newText('#2', '■ (4 x 100€) Cable De Tension En Acier Inox...');
-        card.newText('#3', '■ (8 x 10€) Piquets Ancrage de Sol en Acier...');
+        card.newText('#1', '■ 1 x 1000€ | Poteau De Clôture En Acier Ga...');
+        card.newText('#2', '■ 4 x 100€  | Cable De Tension En Acier Inox...');
+        card.newText('#3', '■ 8 x 10€   | Piquets Ancrage de Sol en Acier...');
 
-        card.newText('#4', '■ (1 x 50€) Ensemble D\'attaches Pour Clôtu...');
-        card.newText('#5', '□ (3 x 200€) Grille De Clôture En Métal De ...');
-        card.newText('#6', '▣ (50 x 5€) Gravier Pour Répartition De Cha...');
-        card.newText('#7', '□ (1 x 400€) Porte De Jardin À Double Batta...');
-        card.newText('#8', '▣ (6 x 150€) Panneau Modulable De Clôture E...');
+        card.newText('#4', '■ 1 x 50€   | Ensemble D\'attaches Pour Clôtu...');
+        card.newText('#5', '□ 3 x 200€  | Grille De Clôture En Métal De ...');
+        card.newText('#6', '▣ 50 x 5€   | Gravier Pour Répartition De Cha...');
+        card.newText('#7', '□ 1 x 400€  | Porte De Jardin À Double Batta...');
+        card.newText('#8', '▣ 6 x 150€  | Panneau Modulable De Clôture E...');
         card.newAction('OPEN_URL_IN_IFRAME', 'Open iFrame', 'https://app.runmorph.dev/embedded-flow');
         card.newAction('OPEN_URL_IN_IFRAME', 'Open in Tab', 'https://runmorph.dev');
 
