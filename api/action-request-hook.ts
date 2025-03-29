cf1f39b7ed97ef326f5ef7b7288e696d041cab0a
Henri <HenriChabrand@users.noreply.github.com>
2025-03-29 14:24:31 +0100

    Test 1.0.20

--- api/action-request-hook.ts
+++ api/action-request-hook.ts
@@ -1,38 +1,41 @@
-import { NowRequest, NowResponse } from '@vercel/node';
-import { Morph } from 'run-morph-client';
+import { NowRequest, NowResponse } from "@vercel/node";
+import { Morph } from "run-morph-client";
 
 export default async (req: NowRequest, res: NowResponse): Promise<void> => {
-    console.log(req.body);
+  console.log(req.body);
 
-    try {
-        const request_id = req.body.id ?? "";
-        if (!request_id) throw new Error("Request ID is missing");
+  try {
+    const request_id = req.body.id ?? "";
+    if (!request_id) throw new Error("Request ID is missing");
 
-        const apiKey = process.env.API_KEY ?? "";
-        const apiSecret = process.env.API_SECRET ?? "";
+    const apiKey = process.env.API_KEY ?? "";
+    const apiSecret = process.env.API_SECRET ?? "";
 
-        if (!apiKey || !apiSecret) throw new Error("API credentials are missing");
+    if (!apiKey || !apiSecret) throw new Error("API credentials are missing");
 
-        let morph = new Morph(apiKey, apiSecret);
+    let morph = new Morph(apiKey, apiSecret);
 
-        if (req.body.type === 'card' && req.body.context?.card_id) {
-            let cardId = req.body.context.card_id ?? "";
-            let cardResponseBuilder = morph.newCardResponseBuilder(request_id);
+    if (req.body.type === "card" && req.body.context?.card_id) {
+      let cardId = req.body.context.card_id ?? "";
+      let actionResponseBuilder = morph.newActionResponseBuilder(request_id);
 
-            let card_succeed = cardId !== 'card_failing';
-            let card_message = `Card ${cardId} processed!`;
+      let action_succeed = cardId !== "card_failing";
+      let action_message = `Card ${cardId} processed!`;
 
-            let success = await cardResponseBuilder.build(card_succeed, card_message);
-            if (success) {
-                res.status(201).json({ message: 'Created' });
-                return;
-            }
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            message: 'An error occurred when processing the request.',
-            error: (error as Error).message
-        });
+      let success = await actionResponseBuilder.build(
+        action_succeed,
+        action_message
+      );
+      if (success) {
+        res.status(201).json({ message: "Created" });
+        return;
+      }
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "An error occurred when processing the request.",
+      error: (error as Error).message,
+    });
+  }
 };
--- api/card-request-hook.ts
+++ api/card-request-hook.ts
@@ -1,76 +1,87 @@
-import { NowRequest, NowResponse } from '@vercel/node';
-import { Morph } from 'run-morph-client'; 
+import { NowRequest, NowResponse } from "@vercel/node";
+import { Morph } from "run-morph-client";
 
 export default async (req: NowRequest, res: NowResponse): Promise<void> => {
-    console.log(req.body)
-    try {
-        const request_id = req.body.id;
-        let morph = new Morph(process.env.API_KEY, process.env.API_SECRET);
-        
-        let cardBuilder = morph.newCardBuilder(request_id, true);
-
-        
-        let card_2 = cardBuilder.newCard('Morph License Agreement');
-        card_2.setLink("https://henri.pm")
-        
-        card_2.newStatus('Status', 'Awaiting Signature', 'WARNING');
-        card_2.newText('Until',  '15/04/24');
-        card_2.newText('Signee',  'Mark Ross');
-        card_2.newText('Contract PDF',  'voir le PDF','https://app.runmorph.dev/embedded-flow');
-        card_2.newText('Owner',  'Henri Chabrand');
-        
-        card_2.newAction('OPEN_URL_IN_IFRAME', 'Edit Contract', 'https://app.runmorph.dev/embedded-flow');
-        card_2.newAction('OPEN_URL', 'Download as PDF', 'https://app.runmorph.dev/embedded-flow');
-       
-        let card = cardBuilder.newCard('NDA Partners (V2.4)');
-        card.setLink("https://henri/pm")
-        
-        card.newStatus('Status', 'Signed', 'SUCCESS');
-        card.newText('Since',  '21/03/24');
-        card.newText('Signee',  'Ron Swanson');
-        card.newText('Owner',  'Henri Chabrand','https://henri.pm');
-        card.newAction('REQUEST', 'Success Action', null, 'action_success');
-           card.newAction('REQUEST', 'Failing Action', null, 'action_failing');
-        //card.newAction('OPEN_URL_IN_IFRAME', 'Edit Contract', 'https://whatwebcando.today/clipboard.html');
-       // card.newAction('OPEN_URL', 'Download as PDF', 'https://runmorph.dev');
-
-        // Action at the root of the card view
-        //cardBuilder.newRootAction('OPEN_URL_IN_IFRAME', 'New Contract', 'https://app.runmorph.dev/embedded-flow');
-
-
-
-        //cardBuilder.newRootAction('OPEN_URL_IN_IFRAME', 'Open in Tab', 'https://runmorph.dev');
-        cardBuilder.newRootAction('REQUEST', 'Success Action', null, 'action_success');
-        cardBuilder.newRootAction('REQUEST', 'Failing Action', null, 'action_failing');
-        
-         
-        let card = cardBuilder.newCard('Test');
-        
-        
-        
-        card.newText('Debug',  'true');
-       
-        card.newAction('OPEN_URL_IN_IFRAME', 'Edit Contract', 'https://app.runmorph.dev/embedded-flow');
-    
-        const card_response = await cardBuilder.build();      
-        return res.status(201).send(card_response);
-        
-        /*let success = await cardBuilder.build();
- 
-        if(success) {
-            return res.status(201).send({ message: 'Created' });
-        }
-        else {
-            return res.status(500).json({ 
-                message: "An error occurred when sending POST request to the API.", 
-                error: "API Request Failed"
-            });
-        }*/
-    } catch (error) {
-        console.log(JSON.stringify(error))
-        return res.status(500).json({ 
-            message: 'An error occurred when fetching the request id or sending post request.', 
-            error: error.message
-        });
+  console.log(req.body);
+  try {
+    const request_id = req.body.id ?? "";
+    if (!request_id) {
+      throw new Error("Request ID is missing");
     }
+
+    const apiKey = process.env.API_KEY ?? "";
+    const apiSecret = process.env.API_SECRET ?? "";
+
+    if (!apiKey || !apiSecret) {
+      throw new Error("API credentials are missing");
+    }
+
+    let morph = new Morph(apiKey, apiSecret);
+    let cardBuilder = morph.newCardBuilder(request_id, true);
+
+    let card2 = cardBuilder.newCard("Morph License Agreement");
+    card2.setLink("https://henri.pm");
+
+    card2.newStatus("Status", "Awaiting Signature", "WARNING");
+    card2.newText("Until", "15/04/24");
+    card2.newText("Signee", "Mark Ross");
+    card2.newText(
+      "Contract PDF",
+      "voir le PDF",
+      "https://app.runmorph.dev/embedded-flow"
+    );
+    card2.newText("Owner", "Henri Chabrand");
+
+    card2.newAction(
+      "OPEN_URL_IN_IFRAME",
+      "Edit Contract",
+      "https://app.runmorph.dev/embedded-flow"
+    );
+    card2.newAction(
+      "OPEN_URL",
+      "Download as PDF",
+      "https://app.runmorph.dev/embedded-flow"
+    );
+
+    let card1 = cardBuilder.newCard("NDA Partners (V2.4)");
+    card1.setLink("https://henri/pm");
+
+    card1.newStatus("Status", "Signed", "SUCCESS");
+    card1.newText("Since", "21/03/24");
+    card1.newText("Signee", "Ron Swanson");
+    card1.newText("Owner", "Henri Chabrand", "https://henri.pm");
+    card1.newAction("REQUEST", "Success Action", undefined, "action_success");
+    card1.newAction("REQUEST", "Failing Action", undefined, "action_failing");
+
+    cardBuilder.newRootAction(
+      "REQUEST",
+      "Success Action",
+      undefined,
+      "action_success"
+    );
+    cardBuilder.newRootAction(
+      "REQUEST",
+      "Failing Action",
+      undefined,
+      "action_failing"
+    );
+
+    let card3 = cardBuilder.newCard("Test");
+    card3.newText("Debug", "true");
+    card3.newAction(
+      "OPEN_URL_IN_IFRAME",
+      "Edit Contract",
+      "https://app.runmorph.dev/embedded-flow"
+    );
+
+    const card_response = await cardBuilder.build();
+    res.status(201).send(card_response);
+  } catch (error) {
+    console.log(JSON.stringify(error));
+    res.status(500).json({
+      message:
+        "An error occurred when fetching the request id or sending post request.",
+      error: (error as Error).message,
+    });
+  }
 };
